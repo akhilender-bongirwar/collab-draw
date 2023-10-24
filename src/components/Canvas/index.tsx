7a2997775863bf6b0e5f82ca55af414acb197c28
Akhilender <akhilenderb9@gmail.com>
2023-10-24 19:46:54 +0530

    Fixed: Background color of Downloaded image
    - Previously the image downloaded on clicking the download button had a
      transparent background and all the colors just showed up as
      transparent background
    - Fixed it by introducing the white background for the canvas board so
      it looks consistent and looks as expected from any paint applications
    - Maintained the code structure as well as the quality.
    
    Signed-off-by: Akhilender <akhilenderb9@gmail.com>

--- src/components/Canvas/index.tsx
+++ src/components/Canvas/index.tsx
@@ -1,4 +1,4 @@
-import { NAV_ITEMS } from "@/constants";
+import { COLORS, NAV_ITEMS } from "@/constants";
 import React, { useEffect, useLayoutEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { actionItemClick } from "@/slice/navSlice";
@@ -8,20 +8,19 @@ const Canvas: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const validToDraw = useRef<Boolean>(false);
 
-  const {activeNavItem, actionNavItem} = useSelector(
+  const { activeNavItem, actionNavItem } = useSelector(
     (state: any) => state.nav
   );
 
   const { color, size } = useSelector(
     (state: any) => state.toolbox[activeNavItem]
   );
-    
+
   useEffect(() => {
     if (!canvasRef.current) return;
     const canvas = canvasRef.current;
-    const context = canvas.getContext("2d");
 
-    if(actionNavItem === NAV_ITEMS.DOWNLOAD) {
+    if (actionNavItem === NAV_ITEMS.DOWNLOAD) {
       const URL = canvas.toDataURL();
       // console.log(URL);
       const anchor = document.createElement("a");
@@ -30,7 +29,17 @@ const Canvas: React.FC = () => {
       anchor.click();
     }
     dispatch(actionItemClick(null));
-  },[actionNavItem, dispatch]);
+  }, [actionNavItem, dispatch]);
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    if (context) {
+      context.fillStyle = COLORS.WHITE;
+      context.fillRect(0, 0, canvas.width, canvas.height);
+    }
+  }, []);
 
   useEffect(() => {
     if (!canvasRef.current) return;
