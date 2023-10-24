b0cdffd11586a16246db39e46aae927d6e83830b
Akhilender <akhilenderb9@gmail.com>
2023-10-24 19:28:21 +0530

    Feature:Implemented Download Feature
    - Now the users can download the drawing just by clicking on the
      download button
    - Handled the nav buttons differently by not putting active state on
      undo and redo and download buttons
    - Every sketch will get downloaded as canvas-image.png.
    - Maintained the code structure as well as the code quality
    
    Signed-off-by: Akhilender <akhilenderb9@gmail.com>

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -18,8 +18,11 @@ const Navbar: React.FC = () => {
   const activeNavItem: NAV_ITEM_KEYS = useSelector(
     (state: any) => state.nav.activeNavItem
   );
-  const handleNavClick = (navItemName : NAV_ITEM_KEYS) => {
-    dispatch(navItemClick(navItemName));
+  const handleNavClick = (navIten : NAV_ITEM_KEYS) => {
+    dispatch(navItemClick(navIten));
+  }
+  const handleActionClick = (actionItem : NAV_ITEM_KEYS) => {
+    dispatch(actionItemClick(actionItem));
   }
   return (
     <div className={styles.navContainer}>
@@ -29,13 +32,13 @@ const Navbar: React.FC = () => {
       <div className={cx(styles.iconContainer, {[styles.active]: activeNavItem === NAV_ITEMS.ERASER})} onClick={() => handleNavClick(NAV_ITEMS.ERASER)}>
         <FontAwesomeIcon icon={faEraser} className={styles.icon} />
       </div>
-      <div className={styles.iconContainer}>
+      <div className={styles.iconContainer} onClick={() => handleActionClick(NAV_ITEMS.UNDO)}>
         <FontAwesomeIcon icon={faUndo} className={styles.icon} />
       </div>
-      <div className={styles.iconContainer}>
+      <div className={styles.iconContainer} onClick={() => handleActionClick(NAV_ITEMS.REDO)}>
         <FontAwesomeIcon icon={faRedo} className={styles.icon} />
       </div>
-      <div className={styles.iconContainer}>
+      <div className={styles.iconContainer} onClick={() => handleActionClick(NAV_ITEMS.DOWNLOAD)}>
         <FontAwesomeIcon icon={faDownload} className={styles.icon} />
       </div>
     </div>
